f2b49bad4cdb2b9c8a1c40a2abf5415ac442df02
tusharsharma2822 <tusharsharma2822@gmail.com>
2024-04-27 10:11:34 +0530

    Postorder traversal of a tree

---
+++ Binary Tree/postorderTraversal.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int key){
+        data = key;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void postorder(Node *root){
+    if(root == nullptr){
+        return;
+    }
+    postorder(root -> left);
+    postorder(root -> right);
+    cout << root -> data << " ";
+}
+
+int main(int argc, char const *argv[])
+{
+    struct Node *root = new Node(1);
+    root -> left = new Node(2);
+    root -> right = new Node(3);
+    root -> left -> left = new Node(4);
+    root -> left -> right = new Node(5);
+    root -> left -> right -> left = new Node(6);
+    root -> right -> left = new Node(7);
+    root -> right -> right = new Node(8);
+    root -> right -> right -> left = new Node(9);
+    root -> right -> right -> right = new Node(10);
+
+    postorder(root);
+
+    return 0;
+}

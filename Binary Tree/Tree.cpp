983d825e7919b53a25a02ad7d84f9f2253884b6c
tusharsharma2822 <tusharsharma2822@gmail.com>
2024-04-27 10:04:51 +0530

    Representation of a tree in C++

---
+++ Binary Tree/Tree.cpp
@@ -0,0 +1,31 @@
+/*Creating a simple tree*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int key){
+        data = key;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    struct Node *root = new Node(1);
+    root -> left = new Node(2);
+    root -> right = new Node(3);
+    root -> left -> left = new Node(4);
+    root -> left -> right = new Node(5);
+    root -> left -> right -> left = new Node(6);
+    root -> right -> left = new Node(7);
+    root -> right -> right = new Node(8);
+    root -> right -> right -> left = new Node(9);
+    root -> right -> right -> right = new Node(10);
+    return 0;
+}
